6df15cc9343497357260cbea1968590bfdf9bfa9
Alberto Blázquez <alberto.blazquez@elastic.co>
2025-02-17 16:27:33 +0100

    Fix display value of filters in SearchBar

--- x-pack/solutions/security/plugins/security_solution/public/asset_inventory/components/filters/filters.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/asset_inventory/components/filters/filters.tsx
@@ -4,18 +4,20 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import React, { useMemo, useCallback } from 'react';
+// import React, { useMemo, useCallback } from 'react';
+import React, { useMemo } from 'react';
 import { EuiSpacer, EuiFlexItem } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { FilterGroup } from '@kbn/alerts-ui-shared/src/alert_filter_controls/filter_group';
 import type { FilterControlConfig } from '@kbn/alerts-ui-shared';
 import type { Filter } from '@kbn/es-query';
-import { createKbnUrlStateStorage, Storage } from '@kbn/kibana-utils-plugin/public';
+// import { createKbnUrlStateStorage, Storage } from '@kbn/kibana-utils-plugin/public';
+import { Storage } from '@kbn/kibana-utils-plugin/public';
 import { ControlGroupRenderer } from '@kbn/controls-plugin/public';
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 import { useDataViewContext } from '../../hooks/data_view_context';
 import { useSpaceId } from '../../../common/hooks/use_space_id';
-import { URL_PARAM_KEY } from '../../../common/hooks/use_url_state';
+// import { URL_PARAM_KEY } from '../../../common/hooks/use_url_state';
 import { ASSET_INVENTORY_INDEX_PATTERN } from '../../constants';
 import { FilterGroupLoading } from './filters_loading';
 import { ASSET_INVENTORY_RULE_TYPE_IDS } from './rule_type_ids';
@@ -54,28 +56,28 @@ export interface FiltersProps {
 export const Filters = ({ onFiltersChange }: FiltersProps) => {
   const { dataView, dataViewIsLoading, dataViewIsRefetching } = useDataViewContext();
   const spaceId = useSpaceId();
-  const history = useHistory();
-  const urlStorage = useMemo(
-    () =>
-      createKbnUrlStateStorage({
-        history,
-        useHash: false,
-        useHashQuery: false,
-      }),
-    [history]
-  );
-  const filterControlsUrlState = useMemo(
-    () =>
-      urlStorage.get<FilterControlConfig[] | undefined>(URL_PARAM_KEY.assetInventory) ?? undefined,
-    [urlStorage]
-  );
+  // const history = useHistory();
+  // const urlStorage = useMemo(
+  //   () =>
+  //     createKbnUrlStateStorage({
+  //       history,
+  //       useHash: false,
+  //       useHashQuery: false,
+  //     }),
+  //   [history]
+  // );
+  // const filterControlsUrlState = useMemo(
+  //   () =>
+  //     urlStorage.get<FilterControlConfig[] | undefined>(URL_PARAM_KEY.assetInventory) ?? undefined,
+  //   [urlStorage]
+  // );
 
-  const setFilterControlsUrlState = useCallback(
-    (newFilterControls: FilterControlConfig[]) => {
-      urlStorage.set(URL_PARAM_KEY.assetInventory, newFilterControls);
-    },
-    [urlStorage]
-  );
+  // const setFilterControlsUrlState = useCallback(
+  //   (newFilterControls: FilterControlConfig[]) => {
+  //     urlStorage.set(URL_PARAM_KEY.assetInventory, newFilterControls);
+  //   },
+  //   [urlStorage]
+  // );
 
   const dataViewSpec = useMemo(
     () =>
@@ -91,22 +93,25 @@ export const Filters = ({ onFiltersChange }: FiltersProps) => {
     [dataView]
   );
 
-  const handleFilterChanges = useCallback(
-    (newFilters: Filter[]) => {
-      const updatedFilters = newFilters.map((filter) => {
-        return {
-          ...filter,
-          meta: {
-            ...filter.meta,
-            disabled: false,
-          },
-        };
-      });
+  // const handleFilterChanges = useCallback(
+  //   (newFilters: Filter[]) => {
+  //     const updatedFilters = newFilters.map((filter) => {
+  //       return {
+  //         ...filter,
+  //         meta: {
+  //           ...filter.meta,
+  //           disabled: false,
+  //         },
+  //         query: {
+  //           ...filter.query,
+  //         },
+  //       };
+  //     });
 
-      onFiltersChange(updatedFilters);
-    },
-    [onFiltersChange]
-  );
+  //     onFiltersChange(updatedFilters);
+  //   },
+  //   [onFiltersChange]
+  // );
 
   if (!spaceId || !dataViewSpec) {
     return null;
@@ -125,14 +130,15 @@ export const Filters = ({ onFiltersChange }: FiltersProps) => {
       <EuiSpacer size="l" />
       <FilterGroup
         dataViewId={dataViewSpec?.id || null}
-        onFiltersChange={handleFilterChanges}
+        // onFiltersChange={handleFilterChanges}
+        onFiltersChange={onFiltersChange}
         ruleTypeIds={ASSET_INVENTORY_RULE_TYPE_IDS}
         Storage={Storage}
         defaultControls={DEFAULT_ASSET_INVENTORY_FILTERS}
         chainingSystem="HIERARCHICAL"
         spaceId={spaceId}
-        controlsUrlState={filterControlsUrlState}
-        setControlsUrlState={setFilterControlsUrlState}
+        // controlsUrlState={filterControlsUrlState}
+        // setControlsUrlState={setFilterControlsUrlState}
         ControlGroupRenderer={ControlGroupRenderer}
         maxControls={4}
       />
